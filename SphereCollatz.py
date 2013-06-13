be24afb269c7454245e275f6499cb8f9cce3d670
Christopher Coney <cjc3259@cs.utexas.edu>
2013-06-13 16:54:52 -0500

    easy answer

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -272,3 +272,46 @@ def collatz_solve (r, w) :
     while collatz_read(r, a) :
         v = collatz_eval(a[0], a[1])
         collatz_print(w, a[0], a[1], v)
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env python
+
+# ------------------------------
+# projects/collatz/RunCollatz.py
+# Copyright (C) 2013
+# Glenn P. Downing
+# -------------------------------
+
+"""
+To run the program
+    % python RunCollatz.py < RunCollatz.in > RunCollatz.out
+    % chmod ugo+x RunCollatz.py
+    % RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+To document the program
+    % pydoc -w Collatz
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+
+# from Collatz import collatz_solve
+
+# ----
+# main
+# ----
+
+collatz_solve(sys.stdin, sys.stdout)
